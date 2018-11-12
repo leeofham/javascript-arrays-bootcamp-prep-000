584436ccf78656483a33bc29a65ebff4c1ba2f4e
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-12 11:44:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,6 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  array = array.slice(-1)
+  array = array.pop;
   return array;
 }
