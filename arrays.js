510cf3309cee4b25ab735deb1368eb96ff0a1807
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-12 11:20:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray([1])
+function addElementToBeginningOfArray([1], "foo"){
+  [1].unshift("foo")
+  return [1]
+}
